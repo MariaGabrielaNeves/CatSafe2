8d98ef93d44d7259acd1ffa945c329adb73f2d8d
Maria Camurça <mariagabriela18122006@gmail.com>
2024-06-21 12:37:35 -0400

    EXEMPLO
    Teste com Carol

--- app/src/main/java/com/example/catsafe/MainActivity.java
+++ app/src/main/java/com/example/catsafe/MainActivity.java
@@ -1,19 +1,13 @@
 package com.example.catsafe;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
-
 import com.example.catsafe2.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 }
